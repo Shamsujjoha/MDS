0714479b2eb05a749fd2d8b1496bef3b04d028c0
Shamsujjoha <cmsjoha@gmail.com>
2020-10-07 15:17:08 +0600

    msz

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function countTouches(event) {
 
     // myElement.value= "closest textbox to touchpoint";
    
-   myElement.focus( {preventScroll:false} ); 
+   myElement.focus( {preventScroll:ture} ); 
    // myElement.scrollIntoView();
 
 
